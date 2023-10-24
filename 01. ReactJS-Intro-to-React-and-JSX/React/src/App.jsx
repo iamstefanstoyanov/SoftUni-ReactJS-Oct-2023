3b18b323e70fe1dec8e1162577b18fd9b43e5894
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-10-24 21:53:47 +0100

    Add files via upload

---
+++ 01. ReactJS-Intro-to-React-and-JSX/React/src/App.jsx
@@ -0,0 +1,32 @@
+import Header from '../components/Header';
+import OurServices from '../components/OurServices';
+import Footer from '../components/Footer';
+import About from '../components/About';
+import Why from '../components/Why';
+import Team from '../components/Team';
+import Client from '../components/Client';
+import Info from '../components/Info';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <OurServices />
+
+      <About />
+
+      <Why />
+
+      <Team />
+
+      <Client />
+
+      <Info />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
