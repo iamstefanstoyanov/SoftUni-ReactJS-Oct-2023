9c474df6760b8a58b9caba9839c7c72f304aaff3
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-11-14 18:30:36 +0000

    Card Css update

--- React JS Project Defense - Regular Exam/TheMovieProject/compoments/Card.jsx
+++ React JS Project Defense - Regular Exam/TheMovieProject/compoments/Card.jsx
@@ -5,12 +5,12 @@ export default function Card({ id, title, relDate, description, image, vote }) {
     <div className='card' key={id}>
       <img src={image} alt='Not Poster Available' />
       <h5>{title}</h5>
-      <p className='relDate'>Release Date: {relDate}</p>
+      <p className='relDate'><span>Release Date:</span> {relDate}</p>
       <p>
         <span>Average Vote:</span> <b>{Number(vote).toFixed(2)}</b> /10
       </p>
       {description ? (
-        <p className='description'>Description: {description}</p>
+        <p className='description'><span>Description:</span> {description}</p>
       ) : null}
       <Link to={`/moviedetails/${id}`}>
         <button className='card-btn' type='button'>
