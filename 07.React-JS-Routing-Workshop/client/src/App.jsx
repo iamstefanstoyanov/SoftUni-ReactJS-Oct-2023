108f5fa571a112c2e20315d856d8b690fb4d9f70
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-11-20 18:19:28 +0000

    setAuth

--- 07.React-JS-Routing-Workshop/client/src/App.jsx
+++ 07.React-JS-Routing-Workshop/client/src/App.jsx
@@ -18,16 +18,23 @@ function App() {
   const [auth, setAuth] = useState({});
   const loginSubmitHandler = async (values) => {
     const result = await authService.login(values.email, values.password);
-    setAuth({ result });
-    navigate(Path.Home)
+    setAuth(result);
+    navigate(Path.Home);
   };
+  const values = {
+    loginSubmitHandler,
+    username: auth.username,
+    email: auth.email,
+    isAuthenticated: !!auth.username,
+  };
+
   return (
-    <AuthContext.Provider value={{ loginSubmitHandler }}>
+    <AuthContext.Provider value={values}>
       <div id='box'>
         <Header />
 
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path={Path.Home} element={<Home />} />
           <Route path='/games' element={<GameList />} />
           <Route path='/games/create' element={<GameCreate />} />
           <Route path='/login' element={<Login />} />
--- 07.React-JS-Routing-Workshop/client/src/components/header/Header.jsx
+++ 07.React-JS-Routing-Workshop/client/src/components/header/Header.jsx
@@ -1,20 +1,32 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../../context/authContext';
 
 export default function Header() {
-    return (
-        <header>
-            <h1><Link className="home" to="/">GamesPlay</Link></h1>
-            <nav>
-                <Link to="/games">All games</Link>
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
-            </nav>
-        </header>
-    );
+  const { isAuthenticated, username } = useContext(AuthContext);
+  return (
+    <header>
+      <h1>
+        <Link className='home' to='/'>
+          GamesPlay
+        </Link>
+      </h1>
+      <nav>
+        <Link to='/games'>All games</Link>
+        {isAuthenticated && (
+          <div id='user'>
+            <Link to='/games/create'>Create Game</Link>
+            <Link to='/logout'>Logout</Link>
+            <span>|  {username}</span>
+          </div>
+        )}
+        {!isAuthenticated && (
+          <div id='guest'>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 }
