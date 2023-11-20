7304669cefd671e88a10cca7a563d5036781849d
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-11-20 17:04:58 +0000

    update on login submit

--- 07.React-JS-Routing-Workshop/client/src/components/login/Login.jsx
+++ 07.React-JS-Routing-Workshop/client/src/components/login/Login.jsx
@@ -1,41 +1,49 @@
-import useForm from '../../../hooks/useForm';
-export default function Login() {
-  const { values, onChange, onSubmit } = useForm({
-    email:'',
-    password:'',
-  });
-  return (
-    <section id='login-page' className='auth'>
-      <form id='login' onSubmit={onSubmit}>
-        <div className='container'>
-          <div className='brand-logo'></div>
-          <h1>Login</h1>
-          <label htmlFor='email'>Email:</label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='Sokka@gmail.com'
-            onChange={onChange}
-            value={values.email}
-          />
+import useForm from "../../../hooks/useForm";
 
-          <label htmlFor='login-pass'>Password:</label>
-          <input
-            type='password'
-            id='login-password'
-            name='password'
-            onChange={onChange}
-            value={values.password}
-          />
-          <input type='submit' className='btn submit' value='Login' />
-          <p className='field'>
-            <span>
-              If you don't have profile click <a href='#'>here</a>
-            </span>
-          </p>
-        </div>
-      </form>
-    </section>
-  );
+const LoginFormKyes = {
+    Email: 'email',
+    Password: 'password',
+};
+
+export default function Login({
+    loginSubmitHandler,
+}) {
+    const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+        [LoginFormKyes.Email]: '',
+        [LoginFormKyes.Password]: '',
+    });
+
+    return (
+        <section id="login-page" className="auth">
+            <form id="login" onSubmit={onSubmit}>
+
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name={LoginFormKyes.Email}
+                        placeholder="Sokka@gmail.com"
+                        onChange={onChange}
+                        value={values[LoginFormKyes.Email]}
+                    />
+
+                    <label htmlFor="login-pass">Password:</label>
+                    <input
+                        type="password"
+                        id="login-password"
+                        name={LoginFormKyes.Password}
+                        onChange={onChange}
+                        value={values[LoginFormKyes.Password]}
+                    />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
+                        <span>If you don't have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
+    );
 }
