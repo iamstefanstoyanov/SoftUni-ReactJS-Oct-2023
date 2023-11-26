e10be9429913c82d4763a639cd1f08060657b399
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-11-26 17:35:34 +0000

    Try catch on async functions

--- React JS Project Defense - Regular Exam/TheMovieProject/services/commentsService.js
+++ React JS Project Defense - Regular Exam/TheMovieProject/services/commentsService.js
@@ -1,12 +1,15 @@
 export const getCurrentMovieComments = async (id) => {
   const url = `http://localhost:3030/data/comments?where=movieId%3D%22${id}%22`;
-
-  const data = await (
-    await fetch(url, {
-      method: 'GET',
-    })
-  ).json();
-  return Object.values(data);
+  try {
+    const data = await (
+      await fetch(url, {
+        method: 'GET',
+      })
+    ).json();
+    return Object.values(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getCurrentUserComments = async (userId) => {
@@ -21,70 +24,83 @@ export const getCurrentUserComments = async (userId) => {
 
 export const deleteComment = async (id) => {
   const token = localStorage.getItem('accessToken');
-
   const url = `http://localhost:3030/data/comments/${id}`;
 
-  const data = await (
-    await fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'X-Authorization': token,
-      },
-    })
-  ).json();
-  return data;
+  try {
+    const data = await (
+      await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'X-Authorization': token,
+        },
+      })
+    ).json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const addComment = async (username, inputs, title, id) => {
   const token = localStorage.getItem('accessToken');
-
   const url = `http://localhost:3030/data/comments`;
-  const body = {
-    username: username,
-    comment: inputs.text,
-    title: title,
-    movieId: id,
-  };
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Authorization': token,
-    },
-    body: JSON.stringify(body),
-  });
-  const result = await response.json();
-  return result;
+  try {
+    const body = {
+      username: username,
+      comment: inputs.text,
+      title: title,
+      movieId: id,
+    };
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Authorization': token,
+      },
+      body: JSON.stringify(body),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getCurrentComment = async (id) => {
   const url = `http://localhost:3030/data/comments/${id}`;
-
-  const data = await (
-    await fetch(url, {
-      method: 'GET',
-    })
-  ).json();
-  return data;
+  try {
+    const data = await (
+      await fetch(url, {
+        method: 'GET',
+      })
+    ).json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const editComment = async (newComment) => {
   const token = localStorage.getItem('accessToken');
   const url = `http://localhost:3030/data/comments/${newComment._id}`;
-  const body = {
-    username: newComment.username,
-    comment: newComment.comment,
-    title: newComment.title,
-    movieId: newComment.movieId,
-  };
-  const response = await fetch(url, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Authorization': token,
-    },
-    body: JSON.stringify(body),
-  });
-  const result = await response.json();
-  return result;
+  try {
+    const body = {
+      username: newComment.username,
+      comment: newComment.comment,
+      title: newComment.title,
+      movieId: newComment.movieId,
+    };
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Authorization': token,
+      },
+      body: JSON.stringify(body),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- React JS Project Defense - Regular Exam/TheMovieProject/services/watchlistService.js
+++ React JS Project Defense - Regular Exam/TheMovieProject/services/watchlistService.js
@@ -1,46 +1,54 @@
 export const deleteFromWatchlist = async (id) => {
   const token = localStorage.getItem('accessToken');
-
   const url = `http://localhost:3030/data/watchlists/${id}`;
 
-  const data = await (
-    await fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'X-Authorization': token,
-      },
-    })
-  ).json();
-  return data;
+  try {
+    const data = await (
+      await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'X-Authorization': token,
+        },
+      })
+    ).json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const addToWatchlist = async (data) => {
   const token = localStorage.getItem('accessToken');
-
   const url = `http://localhost:3030/data/watchlists`;
-  const body = {
-    id: data.id,
-    title: data.title,
-    poster_path: data.poster_path,
-    release_date: data.release_date,
-    vote_average: data.vote_average,
-  };
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Authorization': token,
-    },
-    body: JSON.stringify(body),
-  });
 
-  const result = await response.json();
-  return result;
+  try {
+    const body = {
+      id: data.id,
+      title: data.title,
+      poster_path: data.poster_path,
+      release_date: data.release_date,
+      vote_average: data.vote_average,
+    };
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Authorization': token,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getCurrentUserWatchlist = async (userId) => {
+  const url = `http://localhost:3030/data/watchlists?where=_ownerId%3D%22${userId}%22`;
+  
   try {
-    const url = `http://localhost:3030/data/watchlists?where=_ownerId%3D%22${userId}%22`;
     const data = await (await fetch(url)).json();
     return Object.values(data);
   } catch (error) {
