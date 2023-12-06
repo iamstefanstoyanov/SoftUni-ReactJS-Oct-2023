6262eee11ad42cff8907aa9aea285423c409ee74
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-12-06 21:13:35 +0000

    Deployment update

--- React JS Project Defense - Regular Exam/TheMovieProject/compoments/NotFound.jsx
+++ React JS Project Defense - Regular Exam/TheMovieProject/compoments/NotFound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound(){
     return(
         <>
-        <img style={{ width: '40%'}}className="404" src="./images/404.jpg" alt="404 Not Found" />
+        <img style={{ width: '40%'}}className="404" src="../images/404.jpg" alt="404 Not Found" />
         </>
     )
 }
