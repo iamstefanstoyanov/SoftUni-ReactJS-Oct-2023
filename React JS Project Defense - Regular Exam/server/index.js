3a6b7d8942b0d401aad58bd805bba36a85a51137
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-12-11 22:53:03 +0000

    Temp update

---
+++ React JS Project Defense - Regular Exam/server/index.js
@@ -0,0 +1,13 @@
+const server = require('./server');
+
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
+
+exports.api = onRequest((req, res) => {
+  server.emit('request', req, res);
+});
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- React JS Project Defense - Regular Exam/server/server.js
+++ React JS Project Defense - Regular Exam/server/server.js
@@ -215,8 +215,7 @@
       body,
     };
   }
-
-  function parseBody(req) {
+   function parseBody(req) {
     return new Promise((resolve, reject) => {
       let body = '';
       req.on('data', (chunk) => (body += chunk.toString()));
