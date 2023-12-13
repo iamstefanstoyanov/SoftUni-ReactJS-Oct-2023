a3485432f1ad1314bb659b6b7b84e73855fb142a
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-12-13 20:04:28 +0000

    Build and Deploy new version
    Images fixed

--- React JS Project Defense - Regular Exam/TheMovieProject/compoments/NotFound.jsx
+++ React JS Project Defense - Regular Exam/TheMovieProject/compoments/NotFound.jsx
@@ -1,7 +1,8 @@
+import notFound from '../images/404.jpg';
 export default function NotFound(){
     return(
         <>
-        <img style={{ width: '40%'}}className="404" src="../images/404.jpg" alt="404 Not Found" />
+        <img style={{ width: '40%'}}className="404" src={notFound} alt="404 Not Found" />
         </>
     )
 }
